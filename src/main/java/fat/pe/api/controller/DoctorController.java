a2920c7c57dc6044a43fd6c821ea29136c696b67
hugo <hugogabrieltjj@gmail.com>
2023-06-09 11:11:16 -0300

    created doctors route post

---
+++ src/main/java/fat/pe/api/controller/DoctorController.java
@@ -0,0 +1,16 @@
+package fat.pe.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/doctors")
+public class DoctorController {
+    @PostMapping
+    public void store(@RequestBody String json){
+        System.out.println(json);
+    }
+
+}
